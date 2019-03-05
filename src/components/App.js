d9dcc8cd837a3e05ad564ba6b4177924f0485f3a
Gleb Evgenov <glebevgenov@yandex.ru>
2019-03-05 19:32:50 +0300

    design improvements

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     
     render() {
         return (
-            <div className="App container d-flex flex-column">
+            <div className="App container d-flex flex-column my-5">
+                <h2>Роза ветров</h2>
                 <div className="row">
                     <div className="col-lg-6 d-flex flex-column align-items-center">
                         <div className="p-5">
@@ -45,22 +46,24 @@ class App extends Component {
                             />
                         </div>
                     </div>
-                    <div className="col-lg-6 d-flex flex-row justify-content-center align-items-center py-5">
-                        <Windrose 
-                            size={500}
-                            grid={{
-                                axes: [['В','З'], ['СВВ', 'ЮЗЗ'], ['СВ', 'ЮЗ'], ['СCВ', 'ЮЮЗ'], ['C','Ю'], ['СCЗ', 'ЮЮВ'], ['СЗ', 'ЮВ'], ['СЗЗ', 'ЮВВ']],
-                                scale: { maxValue: 6, step: 1 },
-                                color: 'rgb(105, 105, 105)',
-                            }}
-                            polygon={{
-                                vertexRadius: 4,
-                                lineWidth: 2,
-                                lineColor: 'rgb(240, 128, 128)',
-                                fillColor: 'rgba(240, 128, 128, 0.5)',
-                            }}
-                            values={computeWindroseValues(measurements, 8, true)} 
-                        />
+                    <div className="col-lg-6 d-flex flex-column align-items-center">
+                        <div className="p-5">
+                            <Windrose 
+                                size={500}
+                                grid={{
+                                    axes: [['В','З'], ['СВВ', 'ЮЗЗ'], ['СВ', 'ЮЗ'], ['СCВ', 'ЮЮЗ'], ['C','Ю'], ['СCЗ', 'ЮЮВ'], ['СЗ', 'ЮВ'], ['СЗЗ', 'ЮВВ']],
+                                    scale: { maxValue: 6, step: 1 },
+                                    color: 'rgb(105, 105, 105)',
+                                }}
+                                polygon={{
+                                    vertexRadius: 4,
+                                    lineWidth: 2,
+                                    lineColor: 'rgb(240, 128, 128)',
+                                    fillColor: 'rgba(240, 128, 128, 0.5)',
+                                }}
+                                values={computeWindroseValues(measurements, 8, true)} 
+                            />
+                        </div>
                     </div>
                 </div>
                 <h4>Измерения</h4>
