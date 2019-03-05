f99b39447923d7e91c85c86df3006c4fdf9cd5b7
Gleb Evgenov <glebevgenov@yandex.ru>
2019-03-05 17:04:43 +0300

    design improvements

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,10 @@
 import React, { Component } from 'react'; 
 import measurements from '../measurements';
 import Windrose from './Windrose';
+import { computeWindroseValues } from '../utils';
 
 class App extends Component {
-
-    getMAngleDegrees(tAngleRadians) {
-        const mAngleRadians = Math.PI * 2 - (tAngleRadians + Math.PI / 2) % (Math.PI * 2);
-        if (mAngleRadians === Math.PI * 2) {
-            return 0;
-        }
-        return mAngleRadians * (180 / Math.PI);
-    }
-
-    isMAngleInRange(mAngle, min, max) {
-        if (min > max) {
-            max = max + 360;
-        }
-        return mAngle >= min && mAngle < max;
-    }
-
-    computeWindroseValues([...measurements], axisCount, interpolate = false) {
-        const aStep = Math.PI / axisCount;
-        const mDirs = Array.from({ length: axisCount * 2 }, (v, k) => {
-            const axisAngle = k * aStep;
-            return { 
-                min: this.getMAngleDegrees(axisAngle + aStep / 2),
-                max: this.getMAngleDegrees(axisAngle - aStep / 2),
-                axisIndex: k,
-                value: 0,
-            }
-        }).sort((a, b) => a.min - b.min);
-
-        measurements.sort((a, b) => {
-            return a.deg - b.deg;
-        });
-
-        mDirs.forEach((mDir) => {
-            let values = [];
-            let measurement;
-            while (measurements.length 
-                && this.isMAngleInRange(measurements[0].deg, mDir.min, mDir.max)
-            ) {
-                measurement = measurements.shift();
-                values.push(measurement.speed);
-            }
-            if (values.length) {
-                mDir.value = Math.round(values.reduce((prev, curr) => prev + curr) / values.length * 100) / 100;
-            }
-        });
-
-        let values = mDirs.sort((a, b) => a.axisIndex - b.axisIndex).map((mDir) => {
-            return Math.round(mDir.value * 100) / 100;
-        });
-
-        if (interpolate) {
-            values = values.map((value, i) => {
-                if (!value) {
-                    const iPrev = (mDirs.length + i - 1) % mDirs.length;
-                    const iNext = (mDirs.length + i + 1) % mDirs.length;
-                    if (mDirs[iPrev].value && mDirs[iNext].value) {
-                        return Math.round((mDirs[iPrev].value + mDirs[iNext].value) / 2 * 100) / 100;
-                    }
-                } 
-                return value;
-            });
-        }
-
-        return values;
-    }
-
+    
     render() {
         return (
             <div className="App container d-flex flex-column">
@@ -83,12 +19,12 @@ class App extends Component {
                                     color: 'rgb(105, 105, 105)',
                                 }}
                                 polygon={{
-                                    vertexRadius: 4,
+                                    vertexRadius: 2,
                                     lineWidth: 2,
                                     lineColor: 'rgb(240, 128, 128)',
                                     fillColor: 'rgba(240, 128, 128, 0.5)',
                                 }}
-                                values={this.computeWindroseValues(measurements, 2)} 
+                                values={computeWindroseValues(measurements, 2)} 
                             />
                         </div>
                         <div className="p-5">
@@ -100,12 +36,12 @@ class App extends Component {
                                     color: 'rgb(105, 105, 105)',
                                 }}
                                 polygon={{
-                                    vertexRadius: 4,
+                                    vertexRadius: 3,
                                     lineWidth: 2,
                                     lineColor: 'rgb(240, 128, 128)',
                                     fillColor: 'rgba(240, 128, 128, 0.5)',
                                 }}
-                                values={this.computeWindroseValues(measurements, 4)} 
+                                values={computeWindroseValues(measurements, 4)} 
                             />
                         </div>
                     </div>
@@ -123,7 +59,7 @@ class App extends Component {
                                 lineColor: 'rgb(240, 128, 128)',
                                 fillColor: 'rgba(240, 128, 128, 0.5)',
                             }}
-                            values={this.computeWindroseValues(measurements, 8, true)} 
+                            values={computeWindroseValues(measurements, 8, true)} 
                         />
                     </div>
                 </div>
